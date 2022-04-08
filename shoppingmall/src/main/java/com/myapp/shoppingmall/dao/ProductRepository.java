99dccf521ad8f815b996cf8bb156de82d29e4b58
[sjy] <[tutu8880@naver.com]>
2022-04-08 09:00:07 +0900

    4월 7일

---
+++ shoppingmall/src/main/java/com/myapp/shoppingmall/dao/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.myapp.shoppingmall.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.myapp.shoppingmall.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+	Product findByName(String name);
+
+	Product findByNameAndIdNot(String name, int id);
+
+}
